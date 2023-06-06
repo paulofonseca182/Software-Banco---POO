3ea27a401e30914c3f38d38da7fa5dbbb2ab9674
PauloFonseca <pfonseca.arq@gmail.com>
2023-06-06 13:16:27 -0300

    ajustando o teste da class Email

--- tests/entities/Email.test.ts
+++ tests/entities/Email.test.ts
@@ -5,6 +5,7 @@ describe('Email', () => {
   it('Deve ser possivel criar uma instancia (objeto) de Email', () => {
     const email = new Email('teste@email.com');
     expect(email).to.be.instanceOf(Email);
+    expect(email.email).to.be.equal('teste@email.com');
   });
 
   it('Deve causar um erro ao criar um email invalido', () => {
