00d4c33042c76da393ee1587aa644138c76d0a7d
PauloFonseca <pfonseca.arq@gmail.com>
2023-06-08 21:01:05 -0300

    Testes pagamento Pix

--- tests/entities/PagamentoPix.test.ts
+++ tests/entities/PagamentoPix.test.ts
@@ -5,8 +5,12 @@ import PagamentoPix from '../../src/entities/Pagamentos/PagamentoPix';
 
 describe(`PagamentoPix deve ser possivel criar uma instancia
  e efetivar o pagamentoPix`, () => {
-  const conta1 = new ContaPoupanca(100, new Email('teste@teste.com'), 0.1);
-  const conta2 = new ContaPoupanca(100, new Email('teste1@teste.com'), 0.1);
+  let conta1: ContaPoupanca;
+  let conta2: ContaPoupanca;
+  beforeEach(() => {
+    conta1 = new ContaPoupanca(100, new Email('teste@teste.com'), 0.1);
+    conta2 = new ContaPoupanca(100, new Email('teste1@teste.com'), 0.1);
+  });
   it('Deve ser possivel criar uma instancia (objeto) de PagamentoPix', () => {
     const pagamento = new PagamentoPix(
       {
@@ -17,10 +21,34 @@ describe(`PagamentoPix deve ser possivel criar uma instancia
       },
     );
     expect(pagamento).to.be.instanceOf(PagamentoPix);
+  });
+
+  it('Deve ser possivel efetivar o pagamentoPix', () => {
+    const pagamento = new PagamentoPix(
+      {
+        contaOrigem: conta1,
+        contaDestino: conta2,
+        valor: 50,
+        dataPagamento: new Date(),
+      },
+    );
     expect(conta1.saldo).to.be.equal(100);
     expect(conta2.saldo).to.be.equal(100);
     pagamento.efetivar();
     expect(conta1.saldo).to.be.equal(50);
     expect(conta2.saldo).to.be.equal(150);
+  });
+
+  it(`Nao deve ser possivel efetivar o 
+  pagamentoPix com saldo insuficiente`, () => {
+    const pagamento = new PagamentoPix(
+      {
+        contaOrigem: conta1,
+        contaDestino: conta2,
+        valor: 500,
+        dataPagamento: new Date(),
+      },
+    );
+    expect(pagamento.efetivar()).to.be.equal(console.log('Saldo insuficiente'));
   });
 });
